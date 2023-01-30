e6db563c7a9df60bbbfe7ffce11d979c8f27c76b
JurayevJovohir <javokhake1@gmail.com>
2023-01-30 00:47:28 -0800

    pizdez

--- js/main.js
+++ js/main.js
@@ -7,12 +7,19 @@ const elNumber = document.querySelector(".js-input-contact");
 const array = []
 
 function renderArray(todo) {
+    elList.innerHTML = `
+                <h2 class="form-title">Contacts</h2>
+                <div class="card p-3">
+                    <h3 class="card-title">Lorem, ipsum dolor.</h3>
+                    <p class="card-text">Qarindosh</p>
+                    <a class="card-tel" href="tel:+998902483105">+998 90 248 31 05</a>
+                </div>`;
     for (let i = 0; i < todo.length; i++) {
         const elDiv = document.createElement("div")
         elDiv.className += "card p-3 my-3";
         elDiv.innerHTML += `<h3 class="card-title">${todo[i].split(" ")[0]}</h3>
-                            <p class="card-text">${todo[i].split(" ")[1]}</p>
-                            <a class="card-tel" href="tel:${todo[i].split(" ")[2]}">${todo[i].split(" ")[2]}</a>
+        <p class="card-text">${todo[i].split(" ")[1]}</p>
+        <a class="card-tel" href="tel:${todo[i].split(" ")[2]}">${todo[i].split(" ")[2]}</a>
         `
         elList.appendChild(elDiv);
     }
@@ -22,6 +29,6 @@ elForm.addEventListener("submit", function (evt) {
     evt.preventDefault();
 
     array.push(`${elName.value} ${elRelation.value} ${elNumber.value}`);
-    renderArray(array)
+    renderArray(array);
     elForm.reset();
 }); 
